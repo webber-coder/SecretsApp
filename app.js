d7b1419967aff284cfd4dcc45c41d54892bd081e
Ozge Sanli <ozgezeynep08@gmail.com>
2020-10-28 11:35:21 +0100

    connected with DB,created first user

--- app.js
+++ app.js
@@ -3,12 +3,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.static("public"));
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser: true, useUnifiedTopology: true });
+
+const userSchema = {
+    email: String,
+    password: String
+}
+
+const User = new mongoose.model("User",userSchema);
 
 app.get("/",(req,res)=>{
     res.render("home");
@@ -20,6 +30,20 @@ app.get("/login",(req,res)=>{
 
 app.get("/register",(req,res)=>{
     res.render("register");
+})
+
+app.post("/register",(req,res) =>{
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    newUser.save((err)=>{
+        if(!err){
+            res.render("secrets");
+        }else{
+            res.send(err);
+        }
+    });
 })
 
 
