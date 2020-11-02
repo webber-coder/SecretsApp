bddfc5c1522c51bcc97cabe71d07f02ee00713e0
Ozge Sanli <ozgezeynep08@gmail.com>
2020-11-02 11:18:10 +0100

    has is on encryp is off

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require('mongoose-encryption');
+const md5 = require ("md5");
 
 const app = express();
 
@@ -19,11 +19,6 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-
-const secret = process.env.SECRET;
-
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
-
 const User = new mongoose.model("User",userSchema);
 
 app.get("/",(req,res)=>{
@@ -35,7 +30,7 @@ app.get("/login",(req,res)=>{
 })
 
 app.post("/login",(req,res)=>{
-    User.findOne({email: req.body.username,password: req.body.password},(err,result)=>{
+    User.findOne({email: req.body.username,password: md5(req.body.password)},(err,result)=>{
         if(!err){
             if(result){
                 res.render("secrets")
@@ -55,7 +50,7 @@ app.get("/register",(req,res)=>{
 app.post("/register",(req,res) =>{
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     newUser.save((err)=>{
         if(!err){
